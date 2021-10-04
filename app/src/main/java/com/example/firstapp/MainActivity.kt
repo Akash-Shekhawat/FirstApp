978302dcd6b8f7b89ae4b70a25d74c51f18c071c
Akash-Shekhawat <akash.shekhawat@tothenew.com>
2021-10-04 07:46:10 +0530

    Initial 1exercise commit

---
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.firstapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    private var clkhere: Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+
+}
